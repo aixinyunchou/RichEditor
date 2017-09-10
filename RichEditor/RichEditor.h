a8815f3d77e84f639bb8e3a93ed5993504da0e0e
彭运筹 <pengyunchou@mac-server.local>
2017-09-10 19:14:19 +0800

    Initial Commit

---
+++ RichEditor/RichEditor.h
@@ -0,0 +1,19 @@
+//
+//  RichEditor.h
+//  RichEditor
+//
+//  Created by 彭运筹 on 2017/9/10.
+//  Copyright © 2017年 彭运筹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RichEditor.
+FOUNDATION_EXPORT double RichEditorVersionNumber;
+
+//! Project version string for RichEditor.
+FOUNDATION_EXPORT const unsigned char RichEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichEditor/PublicHeader.h>
+
+
